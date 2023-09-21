3424e415cdf97e5a2e2584900932b1a4c276b09b
craftidev <ivann.codev@gmail.com>
2023-09-21 16:21:45 +0200

    corrige Builder, continue servlet avec validation des data

--- src/main/java/org/encheres/bll/ArticleVendu/ArticleVenduManager.java
+++ src/main/java/org/encheres/bll/ArticleVendu/ArticleVenduManager.java
@@ -1,5 +1,28 @@
 package org.encheres.bll.ArticleVendu;
 
+import java.time.LocalDateTime;
+
+import org.encheres.bo.ArticleVendu;
+import org.encheres.errors.DatabaseException;
+
 public interface ArticleVenduManager {
-    // TODO
+    boolean isValid(
+        String nomArticle,
+        String description,
+        LocalDateTime dateDebutEncheres,
+        LocalDateTime dateFinEncheres,
+        Integer prixInitial,
+        Integer prixVente,
+        String etatVente
+    );
+
+    ArticleVendu add(
+        String nomArticle,
+        String description,
+        LocalDateTime dateDebutEncheres,
+        LocalDateTime dateFinEncheres,
+        Integer prixInitial,
+        Integer prixVente,
+        String etatVente
+    ) throws DatabaseException;
 }
