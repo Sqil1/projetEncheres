52788d78cf159489308242cbd89b503cb49b5bb3
craftidev <ivann.codev@gmail.com>
2023-09-21 15:19:17 +0200

    rebasing annoy me

--- src/main/java/org/encheres/dal/DAOFactory.java
+++ src/main/java/org/encheres/dal/DAOFactory.java
@@ -2,14 +2,13 @@ package org.encheres.dal;
 
 import org.encheres.dal.articleVendu.ArticleVenduDAO;
 import org.encheres.dal.articleVendu.ArticleVenduDAOJdbcImpl;
-import org.encheres.dal.articleVendu.ArticleVenduDAOJdbcImpl;
 
 public class DAOFactory {
 
 	public static UtilisateurDAO getUtilisateurDAO() {
 		return new UtilisateurDAOJdbcImpl();
 	}
-	
+
     public static ArticleVenduDAO getArticleVenduDAO() {
         return new ArticleVenduDAOJdbcImpl();
     }
