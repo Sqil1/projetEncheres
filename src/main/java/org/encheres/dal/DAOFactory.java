4a4b06f4d38114cf55075f1e2bbddd8881cd7643
craftidev <ivann.codev@gmail.com>
2023-09-22 17:39:37 +0200

    rebase avec la bo

--- src/main/java/org/encheres/dal/DAOFactory.java
+++ src/main/java/org/encheres/dal/DAOFactory.java
@@ -1,15 +1,8 @@
 package org.encheres.dal;
 
-import org.encheres.dal.articleVendu.ArticleVenduDAO;
-import org.encheres.dal.articleVendu.ArticleVenduDAOJdbcImpl;
-
 public class DAOFactory {
 
 	public static UtilisateurDAO getUtilisateurDAO() {
 		return new UtilisateurDAOJdbcImpl();
-	}
-
-	public static ArticleVenduDAO getArticleVenduDAO() {
-		return new ArticleVenduDAOJdbcImpl();
 	}
 }
